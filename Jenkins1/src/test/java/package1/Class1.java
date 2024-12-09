ad8a1125ab2488588b71bc7686bef59a10cd75bf
AkhileshRazz <166749596+AkhileshRazz@users.noreply.github.com>
2024-12-09 13:18:43 +0530

    add project commit1

---
+++ Jenkins1/src/test/java/package1/Class1.java
@@ -0,0 +1,18 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	@Test
+	public void method1() {
+		System.out.println("Hello from method1 from class1");
+	}
+	
+	
+	@Test
+	public void method2() {
+		System.out.println("Hello from method2 from class1");
+	}
+	
+	
+}
---
+++ Jenkins1/src/test/java/package1/Class2.java
@@ -0,0 +1,17 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+	@Test
+	public void method1() {
+		System.out.println("Hello from method1 from class2");
+	}
+	
+	
+	@Test
+	public void method2() {
+		System.out.println("Hello from metho2 from class2");
+	}
+}

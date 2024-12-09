690ac73d5eb4a27f188617a412b0efefb33aa534
AkhileshRazz <166749596+AkhileshRazz@users.noreply.github.com>
2024-12-09 17:43:53 +0530

    xxxyyzzzz

--- Jenkins1/src/test/java/package1/Class1.java
+++ Jenkins1/src/test/java/package1/Class1.java
@@ -4,13 +4,13 @@ import org.testng.annotations.Test;
 
 public class Class1 {
 	@Test
-	public void method1() {
+	public void method1Test() {
 		System.out.println("Hello from method1 from class1");
 	}
 	
 	
 	@Test
-	public void method2() {
+	public void method2Test() {
 		System.out.println("Hello from method2 from class1");
 	}
 	

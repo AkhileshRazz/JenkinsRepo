281309c4f18f988163d3188e257ad7eb6f58c5d2
AkhileshRazz <166749596+AkhileshRazz@users.noreply.github.com>
2024-12-10 10:47:26 +0530

    --

--- Jenkins1/src/test/java/package1/Class4Test.java
+++ Jenkins1/src/test/java/package1/Class4Test.java
@@ -2,10 +2,12 @@ package package1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Class4Test {
 
-	public void method1Test() 
+	@Test
+	public void method1() 
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
